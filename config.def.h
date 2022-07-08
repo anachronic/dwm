3d73b8a58b6c7005bf4a9ba878e45715ba22fcdb
Nicolás Salas <nsalas@fastmail.com>
2022-07-08 00:18:56 -0400

    use catpuccin palette with iosevka font
    - based on mocha
    - not too bright
    - still feels very dwm-y and catppuccin-y

--- config.def.h
+++ config.def.h
@@ -5,17 +5,18 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=13:autohint=true:antialias=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_normfg[]       = "#c6d0f5";
+static const char col_normbg[]       = "#1e1e2e";
+static const char col_normbd[]       = "#1e1e2e";
+static const char col_selfg[]        = "#96cdfb";
+static const char col_selbg[]        = "#303446";
+static const char col_selbd[]        = "#96cdfb";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg          bg          border   */
+	[SchemeNorm] = { col_normfg, col_normbg, col_normbg },
+	[SchemeSel]  = { col_selfg,  col_selbg,  col_selbd  },
 };
 
 /* tagging */
@@ -59,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 // ordered in a keyboard oriented manner
